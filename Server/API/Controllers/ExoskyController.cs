d4744470b1113e074580021eeabc3fefb8f40098
Adrian Ściepura <adriansciepura@outlook.com>
2024-10-05 14:41:26 +0200

    Added API project with basic controller

---
+++ Server/API/Controllers/ExoskyController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExoskyController : ControllerBase
+    {
+        [HttpGet("getFile/{fileName}")]
+        public async Task<IActionResult> GetStarData(string fileName)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "data", fileName);
+            if(!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+
+            memory.Position = 0;
+            return File(memory, "application/octet-stream", fileName);
+        }
+    }
+}
---
+++ Server/README.md
@@ -0,0 +1,5 @@
+# SERVER SOLUTION
+
+## API
+Dostępne endpointy:
+- /api/Exosky/getFile/{fileName} - zwraca dany plik (trzeba też podać rozszerzenie pliku) z katalogu data znajdującego się na serwerze
